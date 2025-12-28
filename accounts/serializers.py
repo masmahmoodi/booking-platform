a478850ef68059a55208363420ca66b7cb65f425
Masoud Rahman Mahmoodi <masoudrahmanmahmoodi@Masouds-MacBook-Air.local>
2025-12-27 23:39:38 -0800

    add serializer , register view and register url to phase 3

---
+++ accounts/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "role")
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)  # hashes password correctly
+        user.save()
+        return user
